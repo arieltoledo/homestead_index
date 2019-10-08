7ef3837f905ce9b64b874e8d3e5181d2c009d17e
Ariel Toledo <arieltoledo@gmail.com>
2019-10-08 08:05:02 -0300

    Remove extra chars

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-sudo<?php
+<?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+   require_once __DIR__ . '/vendor/autoload.php';
 
-//Loading Yaml parser package
+   //Loading Yaml parser package
 
-use Symfony\Component\Yaml\Yaml;
+   use Symfony\Component\Yaml\Yaml;
 
-echo "Homestead Projects";
+   echo "Homestead Projects";
 
-$homesteadConfigs = Yaml::parseFile('Homestead.yaml');
+   $homesteadConfigs = Yaml::parseFile('Homestead.yaml');
 
-$sites = $homesteadConfigs['sites'];
+   $sites = $homesteadConfigs['sites'];
 
-foreach ($sites as $site) {
-   echo '<ul>' . '<li>' . '<a target="_blank" href="http://' . $site['map']  . '">' . $site['map']   .  '</a>' . '</li></ul>';
-}
+   foreach ($sites as $site) {
+      echo '<ul>' . '<li>' . '<a target="_blank" href="http://' . $site['map']  . '">' . $site['map']   .  '</a>' . '</li></ul>';
+   }
 
 ?>
